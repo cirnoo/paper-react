79dd2ba211233beb7771cb373073cd120212a2eb
cirnoo <ncysatnaf@gmail.com>
2018-03-25 14:08:45 +0800

    fix test

--- __tests__/paper.test.js
+++ __tests__/paper.test.js
@@ -1,22 +1,17 @@
-'use strict'
+"use strict";
 
-import React from 'react'
-import {shallow} from 'enzyme'
-import {Paper} from 'paper-react'
-const DefinePaper = Paper('div')
-const InitPaper = Paper('div', {stlye: {backgroundColor: '#000'}})
+import React from "react";
+import { shallow } from "enzyme";
+import { Paper } from "paper-react";
+const DefinePaper = Paper("div");
+const InitPaper = Paper("div", { style: { backgroundColor: "#000" } });
 
+it("to be defined", () => {
+  const paper = shallow(<DefinePaper />);
+  expect(paper).toBeDefined();
+});
 
-it('to be defined', ()=> {
-  const paper = shallow(
-    <DefinePaper/>
-  )
-  expect(paper).toBeDefined()
-})
-
-it('can use parameter to initializes the component', () => {
-  const paper = shallow(
-    <DefinePaper />
-  )
-  expect(paper).toBeDefined()
-})
+it("can use parameter to initializes the component", () => {
+  const paper = shallow(<InitPaper />);
+  expect(paper).toBeDefined();
+});
